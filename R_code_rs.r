e5bf62698a16ff70ec9e45a29388b9cb65663364
GiuliaAdeleTuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-04-17 17:53:30 +0200

    Create R_code_rs.r

---
+++ R_code_rs.r
@@ -0,0 +1,8 @@
+# R code for remote sensing data 
+# we downloaded the data 
+# install library raster
+install.packages("raster")
+install.packages("RStoolbox")
+
+# or
+install.packages(c("raster","RStoolbox"))
