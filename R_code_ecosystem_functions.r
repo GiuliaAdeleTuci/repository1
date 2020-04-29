db152aa0dd8356a8b09e7ab46b87b0b81ee91ab0
GiuliaAdeleTuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-04-29 18:04:16 +0200

    Create R_code_ecosystem_functions.r

---
+++ R_code_ecosystem_functions.r
@@ -0,0 +1,76 @@
+# R code to view biomass over the world and calculate changes in ecosystem functions
+# energy
+# chemical cycling
+# proxies
+install.packages("rasterdiv")
+install.packages("rasterVis")
+library(rasterdiv)
+library(rasterVis) 
+
+data(copNDVI)
+plot(copNDVI)
+
+#we need to reclassify the data by removing the data from 253:255 given the NA value
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+levelplot(copNDVI)
+
+copNDVI10 <- aggregate(copNDVI, fact=10)
+levelplot(copNDVI10)
+# to make the map smoother  
+
+#10k pixels together, very coarse low detail 
+copNDVI100 <- aggregate(copNDVI, fact=100)
+levelplot(copNDVI100)
+
+# we are going to make a scenario not sustainable with life and to increase CC in the planet -> great impact 
+
+setwd("/Users/giulia/lab")
+defor1 <- brick("defor1_.jpg.png")
+defor2 <- brick("defor2_.jpg.png")
+
+# if you just put the name of the object in R it gives data about it 
+# band1: NIR
+# band2: red
+# band3: green 
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+par(mfrow=c(1,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+#defor1_.1 etc are the names of the band i can obtain them by sending on R only the name of the object (ex. defor1)
+dvi1 <- defor1$defor1_.jpg.1 - defor1$defor1_.jpg.2
+dvi2 <- defor2$defor2_.jpg.1 - defor2$defor2_.jpg.2
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+par(mfrow=c(1,2))
+plot(dvi1, col=cl)
+plot(dvi2, col=cl)
+
+#let's see the difference in the dvi between the first one and the secondo one 
+difdvi <- dvi1 - dvi2
+dev.off()
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cld) 
+# like this we can see where we have lost the forests. -> we see the loss of ecosystem functions (red parts = loss in biomass and related services)
+
+#to make an histogram 
+hist(difdvi) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
