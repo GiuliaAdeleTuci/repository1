c2cf7df338040a5dc40ba95303a82cb2a2f5763a
GiuliaAdeleTuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-05-20 17:39:13 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -46,6 +46,70 @@ writeRaster(copNDVI, "copNDVI.tif")
 levelplot(faPAR10)
 
 
+######## day 3 
+# linear model between faPAR and NDVI, regression model
+erosion <- c(12, 14, 16, 24, 26, 40, 55, 67)
+hm <- c(30, 100, 150, 200, 260, 340, 460, 600)
+plot(erosion, hm, col="red", pch=19, xlab="erosion", ylab="heavy metals", cex=2)
+
+# let's make a linear model to see if they are related to eachother, we're extimating the slope and the intercept 
+model1 <- lm(hm ~ erosion)
+summary(model1) # to see the information of the model
+abline(model1) # to make the regressione line in the graph 
+
+# let's do the same for the faPar and NDVI 
+setwd("/Users/giulia/lab")
+library(raster)
+library(rasterdiv)
+faPAR10 <- raster("faPAR10.tiff")
+plot(copNDVI)
+
+# let's remove the water values from the copNDVI
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA), right=TRUE)
+
+# now we have two variables (faPAR and copNDVI) and we want to see if they are related
+# first we want to see the amount of cells in each raster, we see that the faPAR10 has a lot of cells so we want to use only a sample 
+library(sf) # to call st_* functions
+random.points <- function(x,n)
+{
+lin <- rasterToContour(is.na(x))
+pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
+}
+pts <- random.points(faPAR10,1000)
+# now we have the random points that we will use (1k) 
+
+copNDVIp <- extract(copNDVI,pts) # to extract data from the image into a table
+faPAR10p <- extract(faPAR10,pts)
+
+plot(copNDVIp,faPAR10p)
+
+model2 <- lm(faPAR10p~copNDVIp)
+summary(model2)
+# we see through the r squared and the p-value that the correlation is not random
+plot(copNDVIp,faPAR10p,col="green")
+abline(model1,col="red")
+# we can see that the two values are quite related with some exceptions (like conifer forests with a lot of biomass and low photosynthesis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
