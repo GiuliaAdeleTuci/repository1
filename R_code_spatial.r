81798416c7e9b022fc36543c70bb47a29f687d5e
Giulia Adele Tuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-05-31 18:55:11 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -1,4 +1,4 @@
-# R code for spatial view of points 
+## R code for spatial view of points 
 # we're going to show other lesson's points in space
 
 library(sp)
@@ -12,13 +12,13 @@ head(meuse)
 
 # we're going to show the points in space since they have coordinates so it is a spatial dataset
 # coordinates and explain we are using x and y 
-# to write ˜ it is alt+n 
+# to write ˜ it is alt+n (on mac)
 coordinates(meuse) = ~x+y 
 
 # plot of coordinates, only to see the points in space  
 plot(meuse)
 
-# plot of the sp package explain also the variable we are going to use
+# plot of the sp package, explain also the variable we are going to use
 spplot(meuse, "zinc") 
 
 # excercise: spatial amount of copper + change the title with main
@@ -28,7 +28,7 @@ spplot(meuse, "copper", main = "Copper Concentration")
 bubble(meuse, "zinc")
 bubble(meuse, "zinc", main = "Zinc Concentration")
 
-#excersise: bubble copper in red
+# excersise: bubble copper in red
 bubble(meuse, "copper", main = "Copper Concentration", col = "red")
 
 ## importing new data
@@ -46,12 +46,12 @@ covid <- read.table("covid_agg.csv", head=TRUE)
 head(covid)
 
 # let's do a plot considering the n of cases per country 
-attach(covid)
+attach(covid) # fist we attach to be able to use the dataset
 plot(country,cases)
 
-#in case you don't attach covid you should write plot(covid$country,covid$cases)
-#to see all the countries we should make the labels of the countries vertical with labels which is called las
-plot(country, cases, las=0) #parallel labels
+# in case you don't attach covid you should write plot(covid$country,covid$cases)
+# to see all the countries we should make the labels of the countries vertical with labels which is called las
+plot(country, cases, las=0) # parallel labels
 plot(country, cases, las=1) # all horizontal labels
 plot(country, cases, las=2) # labels are perpendicular 
 plot(country, cases, las=3) # labels are vertical 
@@ -60,36 +60,36 @@ plot(country, cases, las=3) # labels are vertical
 plot(country, cases, las=3, cex.axis=0.5)
 plot(country, cases, las=3, cex.axis=0.7)
 
-# let's plot spatially using ggplot
+### let's plot spatially using ggplot
 # to make a ggplot you need: data, aesthetic mapping which are the variables, type of symbol we want to use
-#ggplot2
+# ggplot2
 install.packages("ggplot2")
 library(ggplot2) #or require(ggplot2)
 
-#we saved the data SISTEMA 
+# we saved the workspace from last lesson, so we just load it to have the data
 
-#download data from last time 
-#setting of the working directory 
+# download data from last time 
+# setting of the working directory 
 setwd("/Users/giulia/lab")
-#upload the previous workspace, 
+# upload the previous workspace, 
 load("lesson3R.RData")
 
 #to see if there is the previous data using ls which means list 
 ls()
-#covid = works
+# covid is present so the operation worked
 
 library(ggplot2)
-#on the website there are all the variables that you can use 
+# on the website there are all the variables that you can use 
 
 data(mpg)
 head(mpg)
-#key components: data, aes=aestethics, geometry
+# key components: data, aes=aestethics, geometry
 ggplot(mpg,aes(x=displ,y=hwy)) + geom_point()
-#let's change the geometry,use lines connecting points
+# let's change the geometry,use lines connecting points
 ggplot(mpg,aes(x=displ,y=hwy)) + geom_line()
 ggplot(mpg,aes(x=displ,y=hwy)) + geom_polygon()
 
-#let's use this with the covid data, size changes the dimension of the points according to a variable
+# let's use this with the covid data, size changes the dimension of the points according to a variable
 head(covid)
 ggplot(covid,aes(x=lon,y=lat,size=cases)) + geom_point()
 
