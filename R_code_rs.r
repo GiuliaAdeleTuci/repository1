6ba600f84733787e18d9be10b0172a07b8e07aa4
Giulia Adele Tuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-05-31 18:42:24 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,37 +1,37 @@
-# R code for remote sensing data 
+## R code for remote sensing data 
 # we downloaded the data 
 # install library raster
 install.packages("raster")
 install.packages("RStoolbox")
 
-# or
+# or to install them togher 
 install.packages(c("raster","RStoolbox"))
 
- setwd("/Users/giulia/lab")
+setwd("/Users/giulia/lab")
 library(raster)
 
-# we are going to import some images of satellite images of an area of Brasil + we assign a name 
+# we are going to import some images of satellite images of an area of Brasil + we assign a name. since these are satellite images, so they have more layers we use brick
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
 plot(p224r63_2011) # we can see the plot of the different reflectance of bands, now we change the color ramp palette
 cl <- colorRampPalette(c("black","grey","light grey"))(100)
 plot(p224r63_2011, col=cl)
 
-#bands of landsat
+# bands of landsat
 # B1: blue
 # B2: green 
 # B3: red
 # B4: near infrared NIR
 
 # let's plot these 4 bands separetely 
-#multiframe of different plots all together -> function par
-# and make a new col ramp palette, so see colors on R colors()
+# multiframe of different plots all together -> function par
+# and make a new col ramp palette, to see colors on R colors()
 par(mfrow=c(2,2))
 clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
-#and plot $ is used to link symbols 
+# and plot, $ is used to link symbols 
 plot(p224r63_2011$B1_sre, col=clb)
 
-#now the green band
+# now the green band
 clg <- colorRampPalette(c("dark green","green","light green"))(100)
 plot(p224r63_2011$B2_sre, col=clg)
 
@@ -56,7 +56,7 @@ plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 
 # now we are making all the components shift and add first the NIR, remove the blue 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
-# like this we mounted the NIR on top of the red component of the RGB spectrum so all the areas reflecting in the NIR will apear red
+# like this we mounted the NIR on top of the red component of the RGB spectrum so all the areas reflecting in the NIR will apear red (mainly vegetation)
 
 # put the NIR on top of the green component of the RGB
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
@@ -64,9 +64,10 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 # now NIR in the blue component
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
-########
+
+######## second lesson 
 setwd("/Users/giulia/lab")
-#load the R data from last lesson function load
+# load the R data from last lesson function load
 # we are using data from 1988 this time so make a multitemporal comparison 
 library(raster)
 p224r63_1988 <- brick("p224r63_1988_masked.grd")
@@ -76,36 +77,38 @@ plot(p224r63_1988)
 par(mfrow=c(2,1))
 plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
-# we can see the two images together so we can appreciate the changes, however the vegatation is difficult to see the veg 
+# we can see the two images together so we can appreciate the changes, however the vegatation is difficult to see
 # show the same plot but with 432 RGBspace, to see the NIR 
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
-# this function enhances the coulds and in general the noise (why the rivers are greenish), in 1988 the humidity was 
-# higher becuase of the higher functioning of the forest, hence the disturbance. as a result is shows evaportaspiration 
+# this function enhances the clouds and in general the noise (that's why the rivers are greenish), in 1988 the humidity was higher becuase of the higher functioning of the forest, hence the disturbance. 
+# as a result is shows evaportaspiration 
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
 
-#we are going to use the vegetation index
+# we are going to use the vegetation index
 dvi2011 <- (p224r63_2011$B4_sre - p224r63_2011$B3_sre)
 cl <- colorRampPalette(c("purple", "light blue", "pink"))(100)
 plot(dvi2011, col=cl)
 
-# the dvi is not amogeneous, it is sensible to the amount of water as well 
+# the dvi is not omogeneous, it is sensible to the amount of water as well 
 # excersise: dvi for 1988
 dvi1988 <- (p224r63_1988$B4_sre - p224r63_1988$B3_sre)
 cl <- colorRampPalette(c("purple", "light blue", "pink"))(100)
 plot(dvi1988, col=cl)
-# the different is not too high 
+# the difference is not too high 
 # difference from one year to the other
 diff <- dvi2011 - dvi1988
 plot(diff)
 
 # let's see the effect of changing the grain (dimension of the pixels) 
-# aggregate function, aggreagtes pixels to make a coarser grain, fact is the factor that indicates the factor of increase of the pixels.
+# aggregate function, aggregates pixels to make a coarser grain, fact is the argument that indicates the factor of increase of the pixels.
 # this process is called resampling 
 p224r63_2011res <- aggregate(p224r63_2011, fact=10)
 p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+
+# now we plot 
 par(mfrow=c(3,1))
 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
