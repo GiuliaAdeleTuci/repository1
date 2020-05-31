c1dc407192c748b4dd4f6396b5d2e6c6cf98fc1c
Giulia Adele Tuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-05-31 17:54:03 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -1,19 +1,22 @@
 ### diversity measurement
+setwd("/Users/giulia/lab") 
+
 library(raster)
-# now we import the image: raster which imports one single layer and brick which imports the whole image
+# now we import the image: we can use raster which imports one single layer and brick which imports the whole image (different layers at a time)
 snt <- brick("snt_r10.tif")
 plot(snt)
 
+# let's use plotRGB whici is used to plot the layers corresponding to the different color bands 
 #B1 is blue
 #B2 green
 #B3 red
 #B4 NIR
 
-plotRGB(snt, 3, 2, 1, stretch="Lin") # like this we plot is as the human eye would see it
+plotRGB(snt, 3, 2, 1, stretch="Lin") # like this we plot the image as the human eye would see it
 
 plotRGB(snt, 4, 3, 2, stretch="Lin") #NIR on top of red, vegetation is then colored red
 
-pairs(snt)
+pairs(snt) # to produce a matrix of scatterplots 
 
 # for the PCA analysis 
 library(RStoolbox)
@@ -27,18 +30,18 @@ plot(sntpca$map)
 plotRGB(sntpca$map, 1,2,3, stretch="Lin")
 
 # we're making use of the moving window on the PC1 to see the different sd
-# set the moving window 5x5 pixels moving
+# set the moving window of 5x5 pixels
 window <- matrix(1, nrow = 5, ncol = 5)
 window 
 
-# now we apply the window (focal measurement), the functions that can be used are several, we are using the standard deviation 
+# now we apply the window (focal measurement) to the PC1, the functions that can be used are several, we are using the standard deviation 
 sd_snt <- focal(sntpca$map$PC1, w=window, fun=sd)
 
 cl <- colorRampPalette(c('dark blue','green','orange','red'))(100) 
 plot(sd_snt, col=cl)
 # we see low variability in the forest area and higher variability in the areas passing from forest to different ecosystems outside. 
 
-# let's plot together the RGB plot and the one about variability 
+# let's plot together the RGB plot and the one with the sd 
 par(mfrow=c(1,2))
 plotRGB(snt, 4, 3, 2, stretch="Lin")
 plot(sd_snt, col=cl)
@@ -55,7 +58,7 @@ plotRGB(clad,1,2,3, stretch="lin")
 window <- matrix(1, nrow = 3, ncol = 3)
 window
 
-# now we use the focal function, it caluclates the values for the local cells 
+# now we use the focal function 
 # we will apply it to a specific band of the image, we will use the very first band 
 pairs(clad) # to see how much they are correlated 
 
@@ -66,7 +69,7 @@ cladpca <- rasterPCA(clad) # this was we have the first component that expalins 
 # we can use the cladpca to the the data which is the output of the function 
 cladpca 
 
-# let's see how much info is explaine dby the first pca 
+# let's see how much info is explained by the first pca 
 summary(cladpca$map) 
 # 98% is explained bu the first component 
 plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
@@ -74,7 +77,7 @@ plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
 # let's make the measurement of the cladonia through the moving window, to see the diversity 
 sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
 
-PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10) # we aggregate the image wit ha factor of 10 
 sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
 
 par(mfrow=c(1,2))
