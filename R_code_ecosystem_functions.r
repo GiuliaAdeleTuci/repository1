45b41e3605c99ce976b1f16aa158d17e92317c0d
Giulia Adele Tuci <62661816+GiuliaAdeleTuci@users.noreply.github.com>
2020-05-31 18:02:07 +0200

    Update R_code_ecosystem_functions.r

--- R_code_ecosystem_functions.r
+++ R_code_ecosystem_functions.r
@@ -1,4 +1,4 @@
-# R code to view biomass over the world and calculate changes in ecosystem functions
+## R code to view biomass over the world and calculate changes in ecosystem functions
 # energy
 # chemical cycling
 # proxies
@@ -7,7 +7,7 @@ install.packages("rasterVis")
 library(rasterdiv)
 library(rasterVis) 
 
-data(copNDVI)
+data(copNDVI) # A RasterLayer of the global average NDVI value per pixel 
 plot(copNDVI)
 
 #we need to reclassify the data by removing the data from 253:255 given the NA value
@@ -16,16 +16,16 @@ levelplot(copNDVI)
 
 copNDVI10 <- aggregate(copNDVI, fact=10)
 levelplot(copNDVI10)
-# to make the map smoother  
+# to make the map smoother, with lower resolution 
 
-#10k pixels together, very coarse low detail 
+#10k pixels together, very coarse and low detail 
 copNDVI100 <- aggregate(copNDVI, fact=100)
 levelplot(copNDVI100)
 
 # we are going to make a scenario not sustainable with life and to increase CC in the planet -> great impact 
 
 setwd("/Users/giulia/lab")
-defor1 <- brick("defor1_.jpg.png")
+defor1 <- brick("defor1_.jpg.png") # imports different layers at a time 
 defor2 <- brick("defor2_.jpg.png")
 
 # if you just put the name of the object in R it gives data about it 
@@ -36,10 +36,10 @@ plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 
 par(mfrow=c(1,2))
-plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin") # Red-Green-Blue plot of a multi-layered Raster object
 plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 
-#defor1_.1 etc are the names of the band i can obtain them by sending on R only the name of the object (ex. defor1)
+#defor1_.1 etc are the names of the band, i can obtain them by running on R only the name of the object (ex. defor1)
 dvi1 <- defor1$defor1_.jpg.1 - defor1$defor1_.jpg.2
 dvi2 <- defor2$defor2_.jpg.1 - defor2$defor2_.jpg.2
 
@@ -48,7 +48,7 @@ par(mfrow=c(1,2))
 plot(dvi1, col=cl)
 plot(dvi2, col=cl)
 
-#let's see the difference in the dvi between the first one and the secondo one 
+#let's see the difference in the dvi between the first one and the second one 
 difdvi <- dvi1 - dvi2
 dev.off()
 cld <- colorRampPalette(c('blue','white','red'))(100) 
